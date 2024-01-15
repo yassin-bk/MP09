49a4b8c4338a65d40333302eb1e45b2b3bfc66f1
yassin-bk <yassinbekkaoui@iesebre.com>
2024-01-15 19:38:40 +0100

    Sync

--- MP09_PRACT1/vendor/composer/autoload_real.php
+++ MP09_PRACT1/vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit6d3725ec62bddf72099535f884d3a76b
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit6d3725ec62bddf72099535f884d3a76b', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit6d3725ec62bddf72099535f884d3a76b', 'loadClassLoader'));
